1451cb9149f6af6790d1d6cd9b8c6b99b0cccfb4
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-09-09 22:51:12 +0700

    Retrieve place information

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,6 +9,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using GoogleApi;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Places.Details.Request;
+using GoogleApi.Entities.Places.Photos.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
+using System.Threading.Tasks;
+using System;
 
 namespace AirportExplorer.Pages
 {
@@ -16,12 +24,13 @@ namespace AirportExplorer.Pages
     {
         private readonly IHostingEnvironment _hostingEnvironment;
         public string MapboxAccessToken { get; }
+        public string GoogleApiKey { get; }
 
         public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
-
             MapboxAccessToken = configuration["Mapbox:AccessToken"];
+            GoogleApiKey = configuration["Google:ApiKey"];
         }
 
         public void OnGet()
@@ -63,6 +72,66 @@ namespace AirportExplorer.Pages
                 }
                 return new JsonResult(featureCollection);
             }
+        }
+
+        public async Task<IActionResult> OnGetAirportDetail(string name, double latitude, double longitude)
+        {
+            var airportDetail = new AirportDetail();
+
+            // Execute the search request
+            var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
+            {
+                Key = GoogleApiKey,
+                Name = name,
+                Location = new Location(latitude, longitude),
+                Radius = 1000
+            });
+
+            // If we did not get a good response, or the list of results are empty then get out of here
+            if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
+                return new BadRequestResult();
+
+            // Get the first result
+            var nearbyResult = searchResponse.Results.FirstOrDefault();
+            string placeId = nearbyResult.PlaceId;
+            string photoReference = nearbyResult.Photos?.FirstOrDefault()?.PhotoReference;
+            string photoCredit = nearbyResult.Photos?.FirstOrDefault()?.HtmlAttributions.FirstOrDefault();
+
+            // Execute the details request
+            var detailsResonse = await GooglePlaces.Details.QueryAsync(new PlacesDetailsRequest
+            {
+                Key = GoogleApiKey,
+                PlaceId = placeId
+            });
+
+            // If we did not get a good response then get out of here
+            if (!detailsResonse.Status.HasValue || detailsResonse.Status.Value != Status.Ok)
+                return new BadRequestResult();
+
+            // Set the details
+            var detailsResult = detailsResonse.Result;
+            airportDetail.FormattedAddress = detailsResult.FormattedAddress;
+            airportDetail.PhoneNumber = detailsResult.InternationalPhoneNumber;
+            airportDetail.Website = detailsResult.Website;
+
+            if (photoReference != null)
+            {
+                // Execute the photo request
+                var photosResponse = await GooglePlaces.Photos.QueryAsync(new PlacesPhotosRequest
+                {
+                    Key = GoogleApiKey,
+                    PhotoReference = photoReference,
+                    MaxWidth = 400
+                });
+
+                if (photosResponse.Buffer != null)
+                {
+                    airportDetail.Photo = Convert.ToBase64String(photosResponse.Buffer);
+                    airportDetail.PhotoCredit = photoCredit;
+                }
+            }
+
+            return new JsonResult(airportDetail);
         }
     }
 }
